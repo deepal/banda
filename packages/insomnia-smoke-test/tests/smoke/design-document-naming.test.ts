718332737723737536810743499e712ab6a24146
Deepal Jayasekara <dpjayasekara@gmail.com>
2023-10-03 14:00:12 +0100

    fix: fix broken tests

--- packages/insomnia-smoke-test/tests/smoke/design-document-naming.test.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '../../playwright/test';
-
-test('can name design documents', async ({ page }) => {
-    await page.getByRole('button', { name: ' New Document' }).click();
-    await page.getByPlaceholder('my-spec.yaml').fill('jurassic park');
-    await page.getByPlaceholder('my-spec.yaml').press('Enter');
-    await page.getByTestId('project').click();
-    await page.getByLabel('jurassic park').click();
-});
